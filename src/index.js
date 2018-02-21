dbd2a61a41389367a99b5886c35ea5b1de02f8eb
jstrR <srovk22@gmail.com>
2018-02-21 15:00:12 +0300

    100% done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+ 	 let counter = 0;
+  	for (let i = 0; i<preferences.length; i++) {
+	    let k = preferences[i];
+	    let m = preferences[k - 1];
+	    let n = preferences[m - 1];
+	    if (n == i + 1){ 
+	    	counter++;
+	    }
+  	}
+ 	 return Math.floor(counter/3);
 };
+
